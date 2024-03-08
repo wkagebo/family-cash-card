cd60f5e7de500b3a7ce079c306b1f40202c0c737
William Kågebo <william.kagebo@gmail.com>
2024-03-08 15:15:40 +0100

    Build Spring Boot application with Gradle

---
+++ src/main/java/example/familycashcard/FamilyCashCardApplication.java
@@ -0,0 +1,13 @@
+package example.familycashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCashCardApplication.class, args);
+	}
+
+}
